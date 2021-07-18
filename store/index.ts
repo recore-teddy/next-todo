7b4809d4687480febbb91c19beffe80908c1bba0
Teddy Jung <teddy@reware.co.kr>
2021-07-18 22:54:54 +0900

    [Add] Use Redux ToolKit - 리덕스 툴킷으로 리듀서 생성 (createSlice) - initStore 부분 수정

--- store/index.ts
+++ store/index.ts
@@ -1,19 +1,21 @@
-import { createStore, applyMiddleware, combineReducers } from "redux";
+// import { createStore, applyMiddleware, combineReducers } from "redux";
 import { HYDRATE, createWrapper } from "next-redux-wrapper";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import todo from "./todo";
 
 // * with-redux-wrapper 예제를 참고하여 만든 Redux Store
 const rootReducer = combineReducers({
-  todo,
+  todo: todo.reducer,
 });
 
 // * 서버에서 생성한 스토어의 상태를 'HYDRATE' 라는 액션을 통해서 클라이언트에 합쳐주는 작업
 const reducer = (state: any, action: any) => {
   if (action.type === HYDRATE) {
     const nextState = {
-      ...state,
-      ...action.payload,
+      ...state, // use previous state
+      ...action.payload, // apply delta from hydration
     };
+    if (state.count) nextState.count = state.count;
     return nextState;
   }
   return rootReducer(state, action);
@@ -22,18 +24,21 @@ const reducer = (state: any, action: any) => {
 // * 스토어의 타입
 export type RootState = ReturnType<typeof rootReducer>;
 
-// * 미들웨어 적용을 위한 스토어 enhancer (리덕스 데브툴 확장 프로그램을 사용하기 위함)
-const bindMiddleWare = (middleware: any) => {
-  if (process.env.NODE_ENV !== "production") {
-    const { composeWithDevTools } = require("redux-devtools-extension");
-    return composeWithDevTools(applyMiddleware(...middleware));
-  }
-  return applyMiddleware(...middleware);
-};
+// // * 미들웨어 적용을 위한 스토어 enhancer (리덕스 데브툴 확장 프로그램을 사용하기 위함)
+// const bindMiddleWare = (middleware: any) => {
+//   if (process.env.NODE_ENV !== "production") {
+//     const { composeWithDevTools } = require("redux-devtools-extension");
+//     return composeWithDevTools(applyMiddleware(...middleware));
+//   }
+//   return applyMiddleware(...middleware);
+// };
 
 // * 리듀서와 미들웨어로 리덕스 스토어를 만들어 리턴
+// const initStore = () => {
+//   return createStore(reducer, bindMiddleWare([]));
+// };
 const initStore = () => {
-  return createStore(reducer, bindMiddleWare([]));
+  return configureStore({ reducer, devTools: true });
 };
 
 // * 공통 페이지 App 컴포넌트에서 wrapper 로 사용하기 위해,
--- store/todo.ts
+++ store/todo.ts
@@ -1,19 +1,20 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { TodoType } from "../types/todo";
 
-// ? 항상 npm-module-or-app/reducer/ACTION_TYPE 형태의 action 타입을 가져야 합니다.
-// * 액션타입 정의 (reducer명/ACTION_TYPE 으로 대부분의 정의가 가능)
-export const SET_TODO_LIST = "todo/SET_TODO_LIST";
+// // ? 항상 npm-module-or-app/reducer/ACTION_TYPE 형태의 action 타입을 가져야 합니다.
+// // * 액션타입 정의 (reducer명/ACTION_TYPE 으로 대부분의 정의가 가능)
+// export const SET_TODO_LIST = "todo/SET_TODO_LIST";
 
-// ? 항상 모듈의 action 생성자들을 함수형태로 export 해야 합니다.
-// * 액션 생성자 정의 (컴포넌트에서 dispatch(setTodo({})) 를 통해 액션을 실행하면 아래 액션 생성자에서 액션 객체를 리턴)
-export const setTodo = (payload: TodoType[]) => {
-  return {
-    type: SET_TODO_LIST,
-    payload,
-  };
-};
+// // ? 항상 모듈의 action 생성자들을 함수형태로 export 해야 합니다.
+// // * 액션 생성자 정의 (컴포넌트에서 dispatch(setTodo({})) 를 통해 액션을 실행하면 아래 액션 생성자에서 액션 객체를 리턴)
+// export const setTodo = (payload: TodoType[]) => {
+//   return {
+//     type: SET_TODO_LIST,
+//     payload,
+//   };
+// };
 
-export const todoActions = { setTodo };
+// export const todoActions = { setTodo };
 
 interface TodoReduxState {
   todos: TodoType[];
@@ -24,14 +25,29 @@ const initialState: TodoReduxState = {
   todos: [],
 };
 
-// ? 항상 reducer() 란 이름의 함수를 export default 해야 합니다.
-// * 리듀서 (이전 상태와 액션객체를 파라미터로 받아 새로운 상태를 만들어서 반환)
-export default function reducer(state = initialState, action: any) {
-  switch (action.type) {
-    case SET_TODO_LIST:
-      const newState = { ...state, todos: action.payload };
-      return newState;
-    default:
-      return state;
-  }
-}
+const todo = createSlice({
+  name: "todo",
+  initialState,
+  reducers: {
+    // * 투두 변경하기
+    setTodo(state, action: PayloadAction<TodoType[]>) {
+      state.todos = action.payload;
+    },
+  },
+});
+
+export const todoActions = { ...todo.actions };
+
+export default todo;
+
+// // ? 항상 reducer() 란 이름의 함수를 export default 해야 합니다.
+// // * 리듀서 (이전 상태와 액션객체를 파라미터로 받아 새로운 상태를 만들어서 반환)
+// export default function reducer(state = initialState, action: any) {
+//   switch (action.type) {
+//     case SET_TODO_LIST:
+//       const newState = { ...state, todos: action.payload };
+//       return newState;
+//     default:
+//       return state;
+//   }
+// }
