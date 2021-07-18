07d21afdb6e465282e0fdd7d84d9ac6d1cdc2362
Teddy Jung <teddy@reware.co.kr>
2021-07-18 15:01:11 +0900

    [Add] 투두 추가하기 (글작성) UI 생성 - 투두 추가하기 페이지 디자인 - 글작성성

---
+++ pages/todo/add.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { NextPage } from "next";
+import AddTodo from "../../components/AddTodo";
+
+const Todo: NextPage = () => {
+  return <AddTodo />;
+};
+
+export default Todo;
